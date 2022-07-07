457c8d260c193903e5fd42dc358a37c246aa4868
arturo <arturoccompany@gmail.com>
2022-07-06 17:39:36 -0700

    feat: add edgedb client

---
+++ app/db.server.ts
@@ -0,0 +1,9 @@
+import { createClient } from "edgedb";
+
+import e from "~/../dbschema/edgeql-js";
+
+export * from "~/../dbschema/edgeql-js";
+
+const client = createClient();
+
+export { client, e };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
+import type { LoaderFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+import { client, e } from '~/db.server'
+
+export const loader: LoaderFunction = async () => {
+	const msg = await e.str('Hello World!').run(client)
+
+	return { msg }
+}
+
 export default function Index() {
-	return (
-		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	)
+	const { msg } = useLoaderData()
+
+	return <h1>{msg}</h1>
 }
