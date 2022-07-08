e1899dcd68606085fbcd34075753760643a5d913
arturo <arturoccompany@gmail.com>
2022-07-07 19:31:16 -0700

    fix: filter the seed user

--- dbschema/seed.ts
+++ dbschema/seed.ts
@@ -8,7 +8,11 @@ async function seed() {
   const provider = "github";
   const provider_id = "123";
   const user = e.select(e.User, (u) => ({
-    filter: e.op(u.account.provider_id, "=", provider_id),
+    filter: e.op(
+      e.op(u.account.provider, "=", provider),
+      'and',
+      e.op(u.account.provider_id, "=", provider_id)
+    ),
   })).assert_single();
 
   // cleanup the existing database
