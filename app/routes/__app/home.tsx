9dfdae73e16686ac93474e56870c9300bba993e1
arturo <arturoccompany@gmail.com>
2022-07-10 02:00:48 -0700

    feat: add layout

---
+++ app/routes/__app/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		//
+		<div>
+			<div>Home</div>
+			<div className="h-[2000px]" />
+		</div>
+	)
+}
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig, presetUno, presetIcons } from 'unocss'
 
 export default defineConfig({
+  theme: {
+    breakpoints: {
+      sm: '500px',
+      md: '614px',
+      mdl: '1002px',
+      lg: '1024px',
+      lgx: '1092px',
+      xl: '1280px',
+    }
+  },
   presets: [
     presetUno(),
     presetIcons()
