1d3458b437eae84373666a07f578f2d609c709d7
arturo <arturoccompany@gmail.com>
2022-10-13 14:39:26 -0700

    feat: add `ErrorBoundary` to `root`

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import reset from '@unocss/reset/tailwind.css'
 
 import unocss from '~/styles/uno.css'
 import { getUser } from '~/models/user.server'
+import { Button } from '~/components'
 
 export const links: LinksFunction = () => {
 	return [
@@ -32,7 +33,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 	})
 }
 
-export default function App() {
+function Document({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<head>
@@ -40,11 +41,50 @@ export default function App() {
 				<Links />
 			</head>
 			<body>
-				<Outlet />
+				{children}
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	)
+}
+
+export default function App() {
+	return (
+		<Document>
+			<Outlet />
+		</Document>
+	)
+}
+
+export function ErrorBoundary() {
+	return (
+		<Document>
+			<div className="grid h-screen place-items-center">
+				<div className="flex flex-col items-center gap-6">
+					<h1 className="text-6xl font-bold">Oops :(</h1>
+					<p className="text-2xl text-gray-500">
+						an error has occurred on the server
+					</p>
+					<div className="flex gap-2">
+						<a href="/">
+							<Button as="div" variant="fill" color="primary">
+								Go to home
+							</Button>
+						</a>
+						<a
+							href="https://github.com/pokedotdev/twitter-clone"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<Button as="div" icon="github">
+								See repo
+							</Button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</Document>
 	)
 }
