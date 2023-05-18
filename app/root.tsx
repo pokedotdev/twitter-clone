48669c4ee0289185e2e55c4b207cc847cf5af13d
pokedotdev <git@poke.dev>
2023-05-17 18:14:45 -0700

    hotfix: suppress hydration warnings

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 function Document({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
